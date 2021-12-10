e5ee57d8cb61c4dee8b5a48095907dbfcdc8ad99
huseyn memmedov <huseyn.memmedov.66@outlook.com>
2021-12-11 00:47:28 +0400

    BACKEND - In Menu List does't work Filters

---
+++ tomato-web-BackEnd/Tomato BackEnd/Controllers/MenuListController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tomato_BackEnd.DAL;
+using Tomato_BackEnd.ViewModels;
+
+namespace Tomato_BackEnd.Controllers
+{
+    public class MenuListController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MenuListController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            MenuListVM menuListVM = new MenuListVM()
+            {
+                Settings = await _context.Settings.ToListAsync()
+            };
+            return View(menuListVM);
+        }
+    }
+}
--- tomato-web-BackEnd/Tomato BackEnd/wwwroot/src/main.js
+++ tomato-web-BackEnd/Tomato BackEnd/wwwroot/src/main.js
@@ -14,16 +14,34 @@ if ($(':input[type=number]').length) {
         e.preventDefault();
     });
 }
+if ($('.menu-items').length) {
+    const defaultFilter = $('.tagsort-active').attr('data-filter');
+    const $grid = $('.menu-items').isotope({
+        itemSelector: '.menu-item',
+        layoutMode: 'fitRows',
+        filter: defaultFilter
+    });
+
+    $('.menu-tags').on('click', 'span', function () {
+        $('.menu-tags span').removeClass('tagsort-active');
+
+        $(this).toggleClass('tagsort-active');
+
+        const filterValue = $(this).attr('data-filter');
+
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+}
 
 // add Intersection observer api
 const header = document.querySelector('.page-header');
 const nav = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
 
 const opacityNav = function (entries) {
     const [entry] = entries;
-    console.log(entry);
 
     if (!entry.isIntersecting) nav.classList.add('darker-bg');
     else nav.classList.remove('darker-bg');
