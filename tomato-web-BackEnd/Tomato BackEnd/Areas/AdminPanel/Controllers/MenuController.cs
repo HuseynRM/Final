4955e53e14236c1ea27d1ee2b7e0de8261eee5b1
huseyn memmedov <huseyn.memmedov.66@outlook.com>
2021-12-24 18:14:59 +0400

    BACKEND - Added MenuList AdminPanel

---
+++ tomato-web-BackEnd/Tomato BackEnd/Areas/AdminPanel/Controllers/MenuController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tomato_BackEnd.DAL;
+using Tomato_BackEnd.Models;
+
+namespace Tomato_BackEnd.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class MenuController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public MenuController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPageCount = Math.Ceiling(_context.SpecialService.Count() / 4m);
+            List<MenuList> menu =
+                await _context.MenuLists.Include(a=>a.MenuCatagory).Skip((page - 1) * 4).Take(4).ToListAsync();
+            return View(menu);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.Categories = _context.MenuCatagories.ToList();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+    }
+}
