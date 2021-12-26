9b3ee929a5a3a315c4aab02d03cf8b3ffa65820a
huseyn memmedov <huseyn.memmedov.66@outlook.com>
2021-12-26 21:58:05 +0400

    BACKEND - Finish Special and Feature CRUD

--- tomato-web-BackEnd/Tomato BackEnd/Areas/AdminPanel/Controllers/HomeFeatureController.cs
+++ tomato-web-BackEnd/Tomato BackEnd/Areas/AdminPanel/Controllers/HomeFeatureController.cs
@@ -12,11 +12,11 @@ using Tomato_BackEnd.Models;
 namespace Tomato_BackEnd.Areas.AdminPanel.Controllers
 {
     [Area("AdminPanel")]
-    public class HomeSpecialController : Controller
+    public class HomeFeatureController : Controller
     {
         private readonly AppDbContext _context;
         private readonly IWebHostEnvironment _env;
-        public HomeSpecialController(AppDbContext context, IWebHostEnvironment env)
+        public HomeFeatureController(AppDbContext context, IWebHostEnvironment env)
         {
             _context = context;
             _env = env;
@@ -25,61 +25,60 @@ namespace Tomato_BackEnd.Areas.AdminPanel.Controllers
         {
             ViewBag.SelectedPage = page;
             ViewBag.TotalPageCount = Math.Ceiling(_context.HomeSpecials.Count() / 4m);
-            List<HomeSpecial> specials = await _context.HomeSpecials.Skip((page - 1) * 4).Take(4).ToListAsync();
+            List<Features> features = await _context.Features.Skip((page - 1) * 4).Take(4).ToListAsync();
 
-            return View(specials);
+            return View(features);
         }
         public async Task<IActionResult> Edit(int id)
         {
-            HomeSpecial Homespec = await _context.HomeSpecials.FirstOrDefaultAsync(x => x.Id == id);
-            if (Homespec == null)
+            Features features = await _context.Features.FirstOrDefaultAsync(x => x.Id == id);
+            if (features == null)
             {
                 return RedirectToAction("index");
             }
-            return View(Homespec);
+            return View(features);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, HomeSpecial homespec)
+        public async Task<IActionResult> Edit(int id, Features features)
         {
-            HomeSpecial existHome = await _context.HomeSpecials.FirstOrDefaultAsync(x => x.Id == id);
-            if (existHome == null)
+            Features existfeature = await _context.Features.FirstOrDefaultAsync(x => x.Id == id);
+            if (existfeature == null)
             {
                 return RedirectToAction("index");
             }
-            if (homespec.SpecialImgFile != null)
+            if (features.FeatureImgFile != null)
             {
-                if (homespec.SpecialImgFile.ContentType != "image/png" && homespec.SpecialImgFile.ContentType != "image/jpeg")
+                if (features.FeatureImgFile.ContentType != "image/png" && features.FeatureImgFile.ContentType != "image/jpeg")
                 {
                     ModelState.AddModelError("ImageFile", "Jpeg ve ya png formatinda file daxil edilmelidir");
                     return View();
                 }
-                if (homespec.SpecialImgFile.Length > (1024 * 1024) * 5)
+                if (features.FeatureImgFile.Length > (1024 * 1024) * 5)
                 {
                     ModelState.AddModelError("ImageFile", "File olcusu 5mb-dan cox olmaz!");
                     return View();
                 }
                 string rootPath = _env.WebRootPath;
-                var fileName = Guid.NewGuid().ToString() + homespec.SpecialImgFile.FileName;
-                var path = Path.Combine(rootPath, "uploads/HomeSpec", fileName);
+                var fileName = Guid.NewGuid().ToString() + features.FeatureImgFile.FileName;
+                var path = Path.Combine(rootPath, "uploads/Feature", fileName);
                 using (FileStream stream = new FileStream(path, FileMode.Create))
                 {
-                    homespec.SpecialImgFile.CopyTo(stream);
+                    features.FeatureImgFile.CopyTo(stream);
                 }
-                if (existHome.SpecialImgFile != null)
+                if (existfeature.FeatureImgFile != null)
                 {
-                    string existPath = Path.Combine(_env.WebRootPath, "uploads/HomeSpec", existHome.SepialImg);
+                    string existPath = Path.Combine(_env.WebRootPath, "uploads/Feature", existfeature.FeatureImg);
                     if (System.IO.File.Exists(existPath))
                     {
                         System.IO.File.Delete(existPath);
                     }
                 }
-                existHome.SepialImg = fileName;
+                existfeature.FeatureImg = fileName;
             }
-            existHome.SpecialName = homespec.SpecialName;
+            existfeature.Name = features.Name;
 
-            existHome.Text = homespec.Text;
-            existHome.Text1 = homespec.Text1;
+            existfeature.Desc = features.Desc;
 
             await _context.SaveChangesAsync();
             return RedirectToAction("index");
@@ -90,56 +89,56 @@ namespace Tomato_BackEnd.Areas.AdminPanel.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(HomeSpecial homespec)
+        public async Task<IActionResult> Create(Features features)
         {
             if (!ModelState.IsValid)
             {
                 return View();
             }
-            if (homespec.SpecialImgFile != null)
+            if (features.FeatureImgFile != null)
             {
-                if (homespec.SpecialImgFile.ContentType != "image/png" && homespec.SpecialImgFile.ContentType != "image/jpeg")
+                if (features.FeatureImgFile.ContentType != "image/png" && features.FeatureImgFile.ContentType != "image/jpeg")
                 {
                     ModelState.AddModelError("ImageFile", "Jpeg ve ya png formatinda file daxil edilmelidir");
                     return View();
                 }
-                if (homespec.SpecialImgFile.Length > (1024 * 1024) * 5)
+                if (features.FeatureImgFile.Length > (1024 * 1024) * 5)
                 {
                     ModelState.AddModelError("ImageFile", "File olcusu 5mb-dan cox olmaz!");
                     return View();
                 }
                 string rootPath = _env.WebRootPath;
-                var fileName = Guid.NewGuid().ToString() + homespec.SpecialImgFile.FileName;
-                var path = Path.Combine(rootPath, "uploads/HomeSpec", fileName);
+                var fileName = Guid.NewGuid().ToString() + features.FeatureImgFile.FileName;
+                var path = Path.Combine(rootPath, "uploads/Feature", fileName);
                 using (FileStream stream = new FileStream(path, FileMode.Create))
                 {
-                    homespec.SpecialImgFile.CopyTo(stream);
+                    features.FeatureImgFile.CopyTo(stream);
                 }
-                homespec.SepialImg = fileName;
+                features.FeatureImg = fileName;
             }
 
-            await _context.HomeSpecials.AddAsync(homespec);
+            await _context.Features.AddAsync(features);
             await _context.SaveChangesAsync();
             return RedirectToAction("index");
         }
         public async Task<IActionResult> Delete(int id)
         {
-            HomeSpecial restaurantHome = await _context.HomeSpecials.FirstOrDefaultAsync(x => x.Id == id);
-            if (restaurantHome == null)
+            Features features = await _context.Features.FirstOrDefaultAsync(x => x.Id == id);
+            if (features == null)
             {
                 return RedirectToAction("index");
             }
 
-            if (_context.HomeSpecials.Count() == 2)
+            if (_context.Features.Count() == 2)
             {
                 return RedirectToAction("index");
             }
 
             string rootPath = _env.WebRootPath;
-            var path = Path.Combine(rootPath, "uploads/HomeSpec", restaurantHome.SepialImg);
+            var path = Path.Combine(rootPath, "uploads/Feature", features.FeatureImg);
             System.IO.File.Delete(path);
 
-            _context.HomeSpecials.Remove(restaurantHome);
+            _context.Features.Remove(features);
             await _context.SaveChangesAsync();
             return RedirectToAction("index");
         }
