8104b2d2c3c0b8ae12c95be1d9793f62f02496c3
huseyn memmedov <huseyn.memmedov.66@outlook.com>
2022-01-07 17:09:04 +0400

    BACKEND - Added email confirm email and forgot password

--- tomato-web-BackEnd/Tomato BackEnd/Controllers/AccountController.cs
+++ tomato-web-BackEnd/Tomato BackEnd/Controllers/AccountController.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MimeKit;
+using MimeKit.Text;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Tomato_BackEnd.DAL;
@@ -17,12 +23,18 @@ namespace Tomato_BackEnd.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-        public AccountController(AppDbContext context, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager)
+        private readonly IWebHostEnvironment _env;
+        public AccountController(AppDbContext context, 
+            UserManager<AppUser> userManager, 
+            SignInManager<AppUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            IWebHostEnvironment env)
         {
             _context = context;
             _roleManager = roleManager;
             _signInManager = signInManager;
             _userManager = userManager;
+            _env = env;
         }
         #region CreateRole
         //public async Task<IActionResult> CreateRole()
@@ -87,7 +99,101 @@ namespace Tomato_BackEnd.Controllers
             await _userManager.AddToRoleAsync(newUser, "Member");
             await _signInManager.SignInAsync(newUser, true);
 
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Web Tomato", "webtomato31@gmail.com"));
+            message.To.Add(new MailboxAddress(newUser.UserName, newUser.Email));
+            message.Subject = "Email Tesdiqleyin";
+
+            string emailbody = string.Empty;
+
+            using (StreamReader stream = new StreamReader(Path.Combine(_env.WebRootPath, "templates", "emailconfirm.html")))
+            {
+                emailbody = stream.ReadToEnd();
+            }
+
+            string emailconfirmtokent = await _userManager.GenerateEmailConfirmationTokenAsync(newUser);
+
+            string url = Url.Action("confirmemail", "account", new { Id = newUser.Id, token = emailconfirmtokent }, Request.Scheme);
+
+            emailbody = emailbody.Replace("{{fullName}}", $"{newUser.UserName}").Replace("{{url}}", $"{url}");
+
+            message.Body = new TextPart(TextFormat.Html) { Text = emailbody };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("webtomato31@gmail.com", "huseyn123");
+            smtp.Send(message);
+            smtp.Disconnect(true);
+
             return RedirectToAction("index", "home");
+        }
+        public async Task<IActionResult> ConfirmEmail(string Id, string token)
+        {
+            if (string.IsNullOrWhiteSpace(Id) || string.IsNullOrWhiteSpace(token))
+            {
+                return NotFound();
+            }
+
+            AppUser appUser = await _userManager.FindByIdAsync(Id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            IdentityResult identityResult = await _userManager.ConfirmEmailAsync(appUser, token);
+            if (!identityResult.Succeeded)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgetPassword(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return NotFound();
+
+            AppUser appUser = await _userManager.FindByEmailAsync(email);
+
+            if (appUser == null)
+                return NotFound();
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Web Tomato", "webtomato31@gmail.com"));
+            message.To.Add(new MailboxAddress(appUser.UserName, appUser.Email));
+            message.Subject = "Reset Password";
+
+            string emailbody = string.Empty;
+
+            using (StreamReader stream = new StreamReader(Path.Combine(_env.WebRootPath, "templates", "forgetpassword.html")))
+            {
+                emailbody = stream.ReadToEnd();
+            }
+
+            string forgetpasswordtoken = await _userManager.GeneratePasswordResetTokenAsync(appUser);
+
+            string url = Url.Action("changepassword", "account", new { Id = appUser.Id, token = forgetpasswordtoken }, Request.Scheme);
+
+            emailbody = emailbody.Replace("{{fullName}}", $"{appUser.UserName} ").Replace("{{url}}", $"{url}");
+
+            message.Body = new TextPart(TextFormat.Html) { Text = emailbody };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("webtomato31@gmail.com", "huseyn123");
+            smtp.Send(message);
+            smtp.Disconnect(true);
+
+            return View();
         }
         public IActionResult Login()
         {
